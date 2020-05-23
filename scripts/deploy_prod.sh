0511e0b30d6eb71eb96dd63ab5529baa19f8ef04
yoloVoe <mainali.subrat@utexas.edu>
2020-05-23 00:27:05 -0500

    Delete scripts directory

--- scripts/deploy_prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-cd "$(dirname "$0")"
-./rebuild_all.sh
-gcloud compute ssh mainali_subrat@instance-1 --zone us-central1-a --command="sudo systemctl stop ajisen"
-./send_to_gcp.sh
-gcloud compute ssh mainali_subrat@instance-1 --zone us-central1-a --command="sudo systemctl start ajisen"
